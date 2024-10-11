df87f632fa0e21c94b6295528deaaa2fa2fbd375
RafaelOliverSan <153394151+RafaelOliverSan@users.noreply.github.com>
2024-10-11 10:17:51 -0300

    Atualização da pagina usuario_dashboard.php

--- bibliotecaTeste/usuario_dashboard.php
+++ bibliotecaTeste/usuario_dashboard.php
@@ -1,28 +1,240 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 
-   <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="./css/sistema.css">
-     <link rel="preconnect" href="https://fonts.googleapis.com">
-     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;700&display=swap"     rel="stylesheet">
-     <title>Document</title>
-   </head>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href=".//css/usuario_dashboardStyle.css">
+  <title>Galeria</title>
+</head>
 
-   <body>
-       <img class="img-fundo" src="./img/fundo3-login.png" alt="">
-      <div class="container">
-       
+<body>
 
-       <a href="index.php"><img class="btn-sair" src="./img/sair.png" alt="botao sair"></a>
+  <div class="container">
 
-       <h1 class="txt-sistema">Seja bem vindo USUARIO!</h1>
-       <h4>SAIR</h4>
+    <header>
+      <h1>Olá, seja bem vindo a nossa galeria.  </h1>
 
-    </div>
+      <nav>
+        <a href="#suspense">Suspense/Terror</a>
+        <a href="#romance">Romance</a>
+        <a href="#aventura">Aventura</a>
+        <a href="#humor">Humor</a>
+      </nav>
 
-   </body>
+      <div class="btn-sair">
+        <a href="index.php"><img class="btn-sair" src="./img/sair.png" alt="botao sair"></a>
+      </div>
+
+    </header>
+
+
+
+    <main class="galeria-mains">
+      <div class="suspense" id="suspense">
+        <h2>Suspense/Terror</h2>
+
+        <div class="gallery">
+          <div class="image-holder">
+            <a href="https://drive.google.com/drive/folders/1nPSmPwcAPVMa7h2x0o1io6zKtfXAmmSL?usp=sharing" target="_blank">
+              <img src="./img/livro-carrie-estranha.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="https://drive.google.com/drive/folders/1W4hPieVfdD-gHaWi289kfoDJHVmo_AqJ?usp=sharing" target="_blank">
+              <img src="./img/baixar-livro-outsider-stephen-king.webp" alt="img-2">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="https://drive.google.com/drive/folders/1-AIATkPrBAbQ8ZlcTZhXG1IqFQ1DQBKI?usp=sharing" target="_blank">
+              <img src="./img/Baixar-Livro-as-Outras-Pessoas.webp.jpg" alt="img-2">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="https://drive.google.com/drive/folders/1-bCHaNeageq23h2Bkllc6sQQSmJSzF54?usp=sharing" target="_blank">
+              <img src="./img/baixar-livro-ilha.webp" alt="img-">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="https://drive.google.com/drive/folders/1-MdTr1VHw0wSWY0fR6DRlCfupiTeaaET?usp=drive_link" target="_blank">
+              <img src="./img/Baixar-Livro-o-Homem-de-Giz.jpg" alt="img-">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="https://drive.google.com/drive/folders/1-lDIYtUoagEnGwfPaUotlwuSB99H847J?usp=sharing" target="_blank">
+              <img src="./img/baixar-livro-doutor-sono-stephen-king.webp" alt="img-">
+            </a>
+
+          </div>
+
+        </div>
+
+
+      </div>
+
+      <div class="romance" id="romance">
+        <h2>Romance</h2>
+
+        <div class="gallery">
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-este-verao-vai-ser-diferente.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-after-depois-da-promessa.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-mil-coracoes-partidos.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-nao-se-apaixone-por-mim.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-um-amor-nada-tradicional.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-todos-os-caminhos-me-levam-ate-voce.webp" alt="img-1">
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="romance" id="aventura">
+        <h2>Aventura</h2>
+
+        <div class="gallery">
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-ultimo-elo-mortal.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-providencia-do-fogo-as-cronicas-do-trono-de-pedra-bruta.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-sombra-ossos.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-ale-da-ilha-dos-perdidos-melissa-de-la-cruz.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-as-tumbas-de-atuan-ursula-le-guin.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-maquinas-mortais-philip-reeve-em-epub-pdf-mobi-ou-ler-online_large.webp" alt="img-1">
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div class="romance" id="humor">
+        <h2>Humor</h2>
+
+        <div class="gallery">
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-deu-merda.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/baixar-livro-minha-mae-uma-peca.webp" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/Baixar-Livro-Piadas-Nerds.jpg" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/Baixar-Opusculo-a-Parodia.jpg" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/Baixar-Livro-Diario-do-Chaves.jpg" alt="img-1">
+            </a>
+
+          </div>
+
+          <div class="image-holder">
+            <a href="#">
+              <img src="./img/Baixar-Livro-Sou-um-Desastre-Com-as-Mulheres.jpg" alt="img-1">
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+
+      
+
+    </main>
+
+
+  </div>
+
+</body>
 
 </html>
