3041b0ec1f03ecf4243b6e33f23ff39cbb9b9596
RafaelOliverSan <153394151+RafaelOliverSan@users.noreply.github.com>
2024-10-18 16:47:54 -0300

    Atualização da página ExcluirCadastro.php 'foi retirado o botão cancelar exclusão e  colocado um botão de voltar'

--- bibliotecaTeste/ExcluirCadastro.php
+++ bibliotecaTeste/ExcluirCadastro.php
@@ -8,10 +8,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Excluir Cadastro</title>
 <?php require_once('htmlUsuariosCadastrados.php'); ?>
-<a href="UsuariosCadastrados.php" class="w3-display-topmiddle w3-red w3-center w3-padding w3-button" style="text-decoration:none; ">
-    <i class="fa fa-ban" style="font-size:5em"></i>
-    <p style="font-weight:bold;">CANCELAR EXCLUSÃO</p>     
-</a> 
+
+echo '
+            <a href="UsuariosCadastrados.php" class="w3-display-topleft">
+                <i class="fa fa-arrow-circle-left w3-large w3-black w3-button w3-xxlarge"></i>     
+            </a> 
+            ';
+
+
 <div class="w3-padding w3-content w3-text-grey w3-third w3-margin w3-display-middle">
     <h1 class="w3-center w3-black w3-round-large w3-margin">EXLUIR CADASTRO - ID: <?php echo " ".$_GET['id']?> </h1>
     <form action="ExcluirActionBD.php" class="w3-container w" method='post'>
@@ -23,6 +27,7 @@
         <label class="w3-text-white w3-padding" style="font-weight: bold; background-color: black; display: inline-block; width: 20%; padding: 10px;">Telefone</label>
         <input name="txtTelefone" class="w3-input w3-light-grey w3-border" value="<?php echo $_GET['telefone']?>">
         <br>
+        
         <label class="w3-text-white w3-padding" style="font-weight: bold; background-color: black; display: inline-block; width: 20%; padding: 10px;">Email</label>
         <input name="txtEmail" class="w3-input w3-light-grey w3-border" value="<?php echo $_GET['email']?>">
         <br>
